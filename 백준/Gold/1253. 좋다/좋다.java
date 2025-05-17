3401da2efdb411519cd335487f9161f260c867a7
kiteof-park <150502999+kiteof-park@users.noreply.github.com>
2025-05-17 17:25:12 +0900

    [Gold IV] Title: 좋다, Time: 156 ms, Memory: 14904 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -7,46 +7,47 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int A[] = new int[N];
+        int[] A = new int[N];
         for(int i = 0; i < N; i++){
             A[i] = Integer.parseInt(st.nextToken());
         }
         
-        // 2. 투 포인터 - 조건을 만족하는 숫자쌍 찾기이므로 양 끝에서 시작되는 투 포인터 사용
-        Arrays.sort(A);    // 정렬
+        // 2. 투 포인터
+        Arrays.sort(A);
+        int cnt = 0;    // 정답 변수
         
-        int cnt = 0;
-   
         for(int i = 0; i < N; i++){
-            int k = A[i];    // 검사대상 k
+            int target = A[i];
             int start = 0;
             int end = N - 1;
+            
             while(start < end){
-                if(A[start] + A[end] == k){
-                    // 조건 추가 
-                    // A[i]를 만드는데, A[i] 자신 포함X(서로 다른 두 수의 합)
-                    if(start != i && end != i){
-                        cnt++;
-                        break;
-                    }
-                    else if(start == i){
-                        start++;
-                    }
-                    else{    // end == i
-                        end--;
-                    }
+                if(start == i){
+                    start++;
+                    continue;
                 }
-                else if (A[start] + A[end] > k){
+                if(end == i){
+                    end--;
+                    continue;
+                }
+                
+                long sum = A[start] + A[end];
+                
+                if(sum == target){
+                    cnt++;
+                    break;
+                }
+                else if(sum > target){
                     end--;
                 }
-                else{    // A[start] + A[end] < k
+                else {    // sum < target
                     start++;
                 }
-            }  
+            }
         }
+        
         PrintWriter pw = new PrintWriter(System.out);
         pw.println(cnt);
         pw.flush();
-        
     }
 }
